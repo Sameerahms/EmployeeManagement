c0831f41a554063f0bd6781095705b83bbe32a0b
sameerahewamaddumage <sameerahewamaddumage@gmail.com>
2021-04-15 21:24:33 +0530

    1st commit

---
+++ src/main/java/com/hms/model/Employee.java
@@ -0,0 +1,64 @@
+package com.hms.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
+    private int id;
+    private String name;
+    private String email;
+    private String phone;
+    private String imageUrl;
+
+    public Employee() {}
+
+    public Employee(String name, String email, String phone, String imageUrl) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.imageUrl = imageUrl;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
