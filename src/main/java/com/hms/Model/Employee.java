caf9684486e7ef13b5b40483778f603c0b96f880
sameerahewamaddumage <sameerahewamaddumage@gmail.com>
2021-04-16 18:09:50 +0530

    2nd commit

--- src/main/java/com/hms/Model/Employee.java
+++ src/main/java/com/hms/Model/Employee.java
@@ -1,9 +1,10 @@
-package com.hms.model;
+package com.hms.Model;
 
 import javax.persistence.*;
+import java.io.Serializable;
 
 @Entity
-public class Employee {
+public class Employee implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(nullable = false, updatable = false)
@@ -60,5 +61,16 @@ public class Employee {
 
     public void setImageUrl(String imageUrl) {
         this.imageUrl = imageUrl;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", imageUrl='" + imageUrl + '\'' +
+                '}';
     }
 }
